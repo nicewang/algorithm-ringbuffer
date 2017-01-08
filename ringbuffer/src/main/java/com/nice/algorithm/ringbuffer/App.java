34244a3c2daeb14bf0f870de8bbbf07aba14e3bb
unknown <>
2017-01-08 10:32:03 +0800

    ringbuffer

---
+++ ringbuffer/src/main/java/com/nice/algorithm/ringbuffer/App.java
@@ -0,0 +1,13 @@
+package com.nice.algorithm.ringbuffer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
